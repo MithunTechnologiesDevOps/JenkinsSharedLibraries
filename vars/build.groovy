3ca7d17f1071452cb20ad1d59c05964d1b95886e
Mithun Technologies <devopstrainingblr@gmail.com>
2021-05-25 17:39:41 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,3 +1,19 @@
-def call(){
+def call(String mavenGoal){
   
+  if ("${mavenGoal}" == "Clean")
+     {
+       sh "mvn clean"
+     }
+  else if ("${mavenGoal}" == "Compile")
+     {
+       sh "mvn clean compile"
+     }
+  else if ("${mavenGoal}" == "Test")
+     {
+       sh "mvn clean test"
+     }
+   else if ("${mavenGoal}" == "Package")
+      {
+        sh "mvn clean package"
+     }
 }
