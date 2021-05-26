ea0ab539778be200dbea836cc3ddbebb3b9044b9
Mithun Technologies <devopstrainingblr@gmail.com>
2021-05-26 19:46:50 +0530

    Create stages.groovy

---
+++ vars/stages.groovy
@@ -0,0 +1,15 @@
+def call(String stageName){
+  
+  if ("${stageName}" == "Build")
+     {
+       sh "mvn clean package"
+     }
+  else if ("${stageName}" == "SonarQube Report")
+     {
+       sh "mvn clean sonar:sonar"
+     }
+  else if ("${stageName}" == "Upload Into Nexus")
+     {
+       sh "mvn clean deploy"
+     }
+}
